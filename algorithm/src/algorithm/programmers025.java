9ee2ae01e33e4f3c3ce41d3ab35c8ef27dfca347
JinhaakM <maengjhak@gmail.com>
2024-07-11 13:35:18 +0900

    문자열 내림차순으로 배치하기

---
+++ algorithm/src/algorithm/programmers025.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class programmers025 {
+
+	public static void main(String[] args) {
+		Solution025 s=new Solution025();
+		String answer=s.solution("Zbcdefg");
+		System.out.println(answer);
+	}
+
+}
+class Solution025 {
+    public String solution(String s) {
+
+        char[] ch=s.toCharArray();
+        Arrays.sort(ch);
+
+        return new StringBuilder(String.valueOf(ch)).reverse().toString();
+    }
+}
